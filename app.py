fb48a92a0923c98fbda8cc06ca656d22e7794733
servidorcalculadora-bit <servidorcalculadora@gmail.com>
2025-08-23 19:10:44 -0300

    fix: Ajustando coleta de parametros

--- app.py
+++ app.py
@@ -2,10 +2,22 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+def get_all_params():
+    params = {}
+    params.update(request.args.to_dict())
+    params.update(request.form.to_dict())
+
+    if request.is_json:
+        json_data = request.get_json()
+        if json_data:
+            params.update(json_data)
+
+    return params
+
 @app.route("/")
 def hello():
-    nome = request.args.get("nome", "Mundo")
-    return {'resposta' : nome}
+    parametros = get_all_params()
+    return {"parametros": parametros}
 
 if __name__ == "__main__":
     app.run(debug=True)
